f95b04b40437c4e0d285cd77f0b972edfecd78b1
Elijah Aaron <elijahhager20@gmail.com>
2025-09-02 11:03:00 -0600

    Made parameters more consistent

--- source/UGX.h
+++ source/UGX.h
@@ -188,7 +188,7 @@ int UGX_drawSquareRGB(f32 x, f32 y, int width, const f32 color[3]){
     return SUCCESS;
 }
 
-int UGX_drawTriangleRGBA(f32 x0, f32 y0, f32 x1, f32 y1, f32 x2, f32 y2, const u8 color[4]){
+int UGX_drawTriangleRGBA(f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3, const u8 color[4]){
     GX_Begin(GX_TRIANGLES, GX_VTXFMT0, 3);
         GX_Position3f32(x0, y0, 0.0f);
         GX_Color4u8(color[0], color[1], color[2], color[3]);
@@ -203,7 +203,7 @@ int UGX_drawTriangleRGBA(f32 x0, f32 y0, f32 x1, f32 y1, f32 x2, f32 y2, const u
     return SUCCESS;
 }
 
-int UGX_drawTriangleRGB(f32 x0, f32 y0, f32 x1, f32 y1, f32 x2, f32 y2, const f32 color[3]){
+int UGX_drawTriangleRGB(f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3, const f32 color[3]){
     GX_Begin(GX_TRIANGLES, GX_VTXFMT1, 3);
         GX_Position3f32(x0, y0, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
