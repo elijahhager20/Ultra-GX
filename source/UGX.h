5b8a09dbce7bd27d16c0a5ab545490f798a6a49f
elijahhager20 <elijahhager20@gmail.com>
2025-12-27 18:38:21 -0700

    q

--- source/UGX.h
+++ source/UGX.h
@@ -63,6 +63,7 @@ int UGX_consoleInit(void){
     return SUCCESS;
 }
 
+// Draws a singe pixel
 int UGX_drawPointRGBA(f32 x, f32 y, const u8 color[4]){
     GX_Begin(GX_POINTS, GX_VTXFMT0, 1);
         GX_Position3f32(x, y, 0.0f);
@@ -72,6 +73,7 @@ int UGX_drawPointRGBA(f32 x, f32 y, const u8 color[4]){
     return SUCCESS;
 }
 
+// Diddo
 int UGX_drawPointRGB(f32 x, f32 y, const f32 color[3]){
     GX_Begin(GX_POINTS, GX_VTXFMT1, 1);
         GX_Position3f32(x, y, 0.0f);
@@ -83,9 +85,11 @@ int UGX_drawPointRGB(f32 x, f32 y, const f32 color[3]){
 
 int UGX_drawLineRGBA(f32 x1, f32 y1, f32 x2, f32 y2, const u8 color[4]){
     GX_Begin(GX_LINES, GX_VTXFMT0, 2);
+        // Point 1
         GX_Position3f32(x1, y1, 0.0f);
         GX_Color4u8(color[0], color[1], color[2], color[3]);
         
+        // Point 2
         GX_Position3f32(x2, y2, 0.0f);
         GX_Color4u8(color[0], color[1], color[2], color[3]);
     GX_End();
@@ -95,9 +99,11 @@ int UGX_drawLineRGBA(f32 x1, f32 y1, f32 x2, f32 y2, const u8 color[4]){
 
 int UGX_drawLineRGB(f32 x1, f32 y1, f32 x2, f32 y2, const f32 color[3]){
     GX_Begin(GX_LINES, GX_VTXFMT1, 2);
+        // Point 1
         GX_Position3f32(x1, y1, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
         
+        // Point 2
         GX_Position3f32(x2, y2, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
     GX_End();
@@ -173,19 +179,19 @@ int UGX_drawSquareRGBA(f32 x, f32 y, int width, const u8 color[4]){
 
 int UGX_drawSquareRGB(f32 x, f32 y, int width, const f32 color[3]){
     GX_Begin(GX_QUADS, GX_VTXFMT1, 4);
-        // Top-left
+        // Point 1
         GX_Position3f32(x, y, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
 
-        // Top-right
+        // Point 2
         GX_Position3f32(x + width, y, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
 
-        // Bottom-right
+        // Point 3
         GX_Position3f32(x + width, y - width, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
 
-        // Bottom-left
+        // Point 4
         GX_Position3f32(x, y - width, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
     GX_End();
@@ -195,12 +201,15 @@ int UGX_drawSquareRGB(f32 x, f32 y, int width, const f32 color[3]){
 
 int UGX_drawTriangleRGBA(f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3, const u8 color[4]){
     GX_Begin(GX_TRIANGLES, GX_VTXFMT0, 3);
+        // Point 1
         GX_Position3f32(x1, y1, 0.0f);
         GX_Color4u8(color[0], color[1], color[2], color[3]);
 
+        // Point 2
         GX_Position3f32(x2, y2, 0.0f);
         GX_Color4u8(color[0], color[1], color[2], color[3]);
 
+        // Point 3
         GX_Position3f32(x3, y3, 0.0f);
         GX_Color4u8(color[0], color[1], color[2], color[3]);
     GX_End();
@@ -210,12 +219,15 @@ int UGX_drawTriangleRGBA(f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3, const u
 
 int UGX_drawTriangleRGB(f32 x1, f32 y1, f32 x2, f32 y2, f32 x3, f32 y3, const f32 color[3]){
     GX_Begin(GX_TRIANGLES, GX_VTXFMT1, 3);
+        // Point 1
         GX_Position3f32(x1, y1, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
 
+        // Point 2
         GX_Position3f32(x2, y2, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
 
+        // Point 3
         GX_Position3f32(x3, y3, 0.0f);
         GX_Color3f32(color[0], color[1], color[2]);
     GX_End();
